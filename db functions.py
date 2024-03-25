96877c708c80edb30069d9d261c93f7a9442045f
charlottepanck <20167@burnside.school.nz>
2024-03-26 10:26:43 +1300

    fetch specific author function

--- db functions.py
+++ db functions.py
@@ -325,31 +325,49 @@ def fetch_books_by_specific_author(book_id):
     db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
 
-    # forename column format
-    sql1 = "SELECT name FROM Author ORDER BY Length(name) desc LIMIT 1;"
+    # author id
+    sqla = f"SELECT author FROM  Books WHERE book_id = {book_id};"
+    cursor.execute(sqla)
+    results = cursor.fetchone()
+    for i in results:
+        authorid = i
+
+    # title column format
+    sql1 = "SELECT title FROM books ORDER BY Length(name) desc LIMIT 1;"
     cursor.execute(sql1)
     results = cursor.fetchone()
     for i in results:
         x = (f"{i}")
-        alg = len(x)
-        asp = (alg-6) * " "
+        tlg = len(x)
+        tsp = (tlg-5) * " "
 
-    # title column format
-    sql2 = f"SELECT title FROM books WHERE author == {int(book)} ORDER BY Length(title) desc LIMIT 1;"
+    # author column format
+    sql2 = f"SELECT name FROM author WHERE author == {authorid} ORDER BY Length(name) desc LIMIT 1;"
     cursor.execute(sql2)
     results = cursor.fetchone()
     for i in results:
         x = (f"{i}")
-        tlg = len(x)
-        tsp = (tlg-10) * " "
+        alg = len(x)
+        asp = (alg-10) * " "
 
-    # print table
-    sql = f"""SELECT Author.name, Books.title FROM Books LEFT JOIN Author ON Books.author = Author.id WHERE Books.book_id = {book_id};"""
-    cursor.execute(sql)
+    # books by author
+    sqlb = f"""SELECT Books.title, Author.name, series.series_title, books.availability 
+FROM Books
+LEFT JOIN Author ON Books.author = Author.id
+LEFT JOIN Series ON Books.series = Series.id
+WHERE author = {authorid};"""
+    cursor.execute(sqlb)
     results = cursor.fetchall()
-    print(f"Author {asp}| Book Title {tsp}")
     for i in results:
-        print(f"{i[0]:{alg}} | {i[1]:{tlg}}")
+        print(i)
+    
+    # print table
+    #sql = f"""SELECT Author.name, Books.title FROM Books LEFT JOIN Author ON Books.author = Author.id WHERE Books.book_id = {book_id};"""
+    #cursor.execute(sql)
+    #results = cursor.fetchall()
+    #print(f"Author {asp}| Book Title {tsp}")
+    #for i in results:
+    #    print(f"{i[0]:{alg}} | {i[1]:{tlg}}")
     db.close()
 
 
@@ -387,8 +405,7 @@ Enter 'c' to view books in this series
         if userinput3 == 'a':
             fetch_specfic_borrowing_table(book)
         if userinput3 == 'b':
-            print("[Table of more Books by this Author]")
-            #fetch_books_by_specific_author(book)
+            fetch_books_by_specific_author(book)
         if userinput3 == 'c':
             print("[Table of books in this series]")
 
