b7687059e945de135846dfd1dc06c0d9f6236d19
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-26 09:34:24 +1300

    new function

--- db functions.py
+++ db functions.py
@@ -72,6 +72,100 @@ def fetch_all_books():
     db.close()
 
 
+def fetch_all_stand_alone_books():
+    db = sqlite3.connect('PraticeHome.db')
+    cursor = db.cursor()
+
+    # title column format
+    sql1 = "SELECT Title FROM Books WHERE Series = 7 ORDER BY Length(title) desc LIMIT 1;"
+    cursor.execute(sql1)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        tlg = len(x)
+        tsp = (tlg-5) * " "
+
+    # author column format
+    sql2 = "SELECT name FROM author ORDER BY Length(name) desc LIMIT 1;"
+    cursor.execute(sql2)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        alg = len(x)
+        asp = (alg-6) * " "
+
+    # series column format
+    sql3 = """SELECT series_title FROM series
+    ORDER BY Length(series_title) desc LIMIT 1;"""
+    cursor.execute(sql3)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        slg = len(x)
+        ssp = (slg-6) * " "
+
+    # print table
+    sql = """SELECT Books.book_id, Books.Title,
+    Author.name, Series.series_title, Books.availability
+    FROM Books
+    LEFT JOIN Author ON Books.Author = Author.id
+    LEFT JOIN Series ON Books.series = Series.id
+    WHERE Series = 7;"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(f"Book ID | Title {tsp}| Author {asp}| Series {ssp}| Avaiability")
+    for i in results:
+        print(f"{i[0]:>7} | {i[1]:{tlg}} | {i[2]:{alg}} | {i[3]:{slg}} | {i[4]}")
+    db.close()
+
+
+def fetch_all_avaliable_books():
+    db = sqlite3.connect('PraticeHome.db')
+    cursor = db.cursor()
+
+    # title column format
+    sql1 = "SELECT Title FROM Books WHERE availability = 'Avaliable' ORDER BY Length(title) desc LIMIT 1;"
+    cursor.execute(sql1)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        tlg = len(x)
+        tsp = (tlg-5) * " "
+
+    # author column format
+    sql2 = "SELECT name FROM author ORDER BY Length(name) desc LIMIT 1;"
+    cursor.execute(sql2)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        alg = len(x)
+        asp = (alg-6) * " "
+
+    # series column format
+    sql3 = """SELECT series_title FROM series
+    ORDER BY Length(series_title) desc LIMIT 1;"""
+    cursor.execute(sql3)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        slg = len(x)
+        ssp = (slg-6) * " "
+
+    # print table
+    sql = """SELECT Books.book_id, Books.Title,
+    Author.name, Series.series_title, Books.availability
+    FROM Books
+    LEFT JOIN Author ON Books.Author = Author.id
+    LEFT JOIN Series ON Books.series = series.id
+    WHERE availability = 'Avaliable';"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(f"Book ID | Title {tsp}| Author {asp}| Series {ssp}| Avaiability")
+    for i in results:
+        print(f"{i[0]:>7} | {i[1]:{tlg}} | {i[2]:{alg}} | {i[3]:{slg}} | {i[4]}")
+    db.close()
+
+
 def fetch_all_members():
     db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
@@ -148,7 +242,7 @@ def fetch_specific_book(book_id):
     results = cursor.fetchall()
     print(f"Book ID | Title {tsp}| Author {asp}| Series {ssp}| Avaiability")
     for i in results:
-        print(f"""{i[0]:>7} | {i[1]:{tlg}} | {i[2]:{alg}} | {i[3]:{slg}} | {i[4]}""")
+        print(f"{i[0]:>7} | {i[1]:{tlg}} | {i[2]:{alg}} | {i[3]:{slg}} | {i[4]}")
     db.close()
 
 
@@ -201,7 +295,7 @@ def fetch_specfic_borrowing_table(book_id):
     for i in results:
         x = (f"{i}")
         fnlg = len(x)
-        fnsp = (fnlg-8) * " "
+        fnsp = (fnlg-6) * " "
 
     # title column format
     sql2 = "SELECT title FROM books ORDER BY Length(title) desc LIMIT 1;"
@@ -214,17 +308,50 @@ def fetch_specfic_borrowing_table(book_id):
 
     # print table
     sql = f"""SELECT Members.forename, Books.title,
-    Borrowing.issue_date, Borrowing.due_date
+    borrowing.issue_date, Borrowing.due_date
     FROM Borrowing
     LEFT JOIN Members ON borrowing.member_id = Members.member_id
     LEFT JOIN Books ON borrowing.book_id = Books.book_id
-    WHERE book_id = {book_id};"""
+    WHERE Borrowing.book_id = {book};"""
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(f"Forename {fnsp}| Book Title {tsp}| Due Date | Issue Date")
+    print(f"Member {fnsp}| Book Title {tsp}| Due Date | Issue Date")
     for i in results:
         print(f"{i[0]:{fnlg}} | {i[1]:{tlg}} | {i[2]:} | {i[3]} ")
     db.close()
+
+
+def fetch_books_by_specific_author(book_id):
+    db = sqlite3.connect('PraticeHome.db')
+    cursor = db.cursor()
+
+    # forename column format
+    sql1 = "SELECT name FROM Author ORDER BY Length(name) desc LIMIT 1;"
+    cursor.execute(sql1)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        alg = len(x)
+        asp = (alg-6) * " "
+
+    # title column format
+    sql2 = f"SELECT title FROM books WHERE author == {int(book)} ORDER BY Length(title) desc LIMIT 1;"
+    cursor.execute(sql2)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        tlg = len(x)
+        tsp = (tlg-10) * " "
+
+    # print table
+    sql = f"""SELECT Author.name, Books.title FROM Books LEFT JOIN Author ON Books.author = Author.id WHERE Books.book_id = {book_id};"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(f"Author {asp}| Book Title {tsp}")
+    for i in results:
+        print(f"{i[0]:{alg}} | {i[1]:{tlg}}")
+    db.close()
+
 
 
 # main code
@@ -232,26 +359,41 @@ print("""
 Welcome to Libaray Database
 
 Enter 1 to view a list of all books
-Enter 2 to view a list of books borrowed by a specific member
-Enter 3 to view a list of members who borrowed a specific book
+Enter 2 to search by Author
+Enter 3 to search by Series
 Enter ... [Add and remove data, Edit/change data, Delete items,
 Renew book, mark a book overdue (can i make this automatic?)]
 """)
-userinput = input('>> ').lower()
-if userinput == '1':
+userinput1 = input('>> ').lower()
+if userinput1 == '1':
     fetch_all_books()
     print("""
-Enter Book ID to view details (e.g. for The Great Gatsby input '50')
-""")
-    book = input(">> ")
-    fetch_specific_book(book)
-    print("""
+Enter 'a' to view details on specific book
+Enter 'b' to view all available books
+Enter 'c' to view all stand alone books
+Enter 'd' to view... (genre, publishing year, rating)
+    """)
+    userinput2 = input('>> ').lower()
+    if userinput2 == 'a':
+        print("\nEnter Book ID to view details (e.g. for The Great Gatsby input '50')\n")
+        book = input(">> ")
+        fetch_specific_book(book)
+        print("""
 Enter 'a' to view avaiability details
-Enter 'b' to view more books by author
-Enter 'c' to view books in series
+Enter 'b' to view more books by this author
+Enter 'c' to view books in this series
 """)
-    userinput = input('>> ').lower()
-    if userinput == 'a':
-        fetch_specfic_borrowing_table(book)
-    if userinput == 'b':
-        print('a')
+        userinput3 = input('>> ').lower()
+        if userinput3 == 'a':
+            fetch_specfic_borrowing_table(book)
+        if userinput3 == 'b':
+            print("[Table of more Books by this Author]")
+            #fetch_books_by_specific_author(book)
+        if userinput3 == 'c':
+            print("[Table of books in this series]")
+
+#    if userinput2 == 'b':
+#        fetch_all_avaliable_books()
+
+#    if userinput2 == 'c':
+#        fetch_all_stand_alone_books()
