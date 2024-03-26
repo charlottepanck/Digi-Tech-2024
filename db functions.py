b31bf95a9deee266178acadd2dcf563095e67f10
charlottepanck <20167@burnside.school.nz>
2024-03-26 19:20:53 +1300

    fetch books by specific author function

--- db functions.py
+++ db functions.py
@@ -333,7 +333,7 @@ def fetch_books_by_specific_author(book_id):
         authorid = i
 
     # title column format
-    sql1 = "SELECT title FROM books ORDER BY Length(name) desc LIMIT 1;"
+    sql1 = "SELECT title FROM books ORDER BY Length(title) desc LIMIT 1;"
     cursor.execute(sql1)
     results = cursor.fetchone()
     for i in results:
@@ -341,14 +341,24 @@ def fetch_books_by_specific_author(book_id):
         tlg = len(x)
         tsp = (tlg-5) * " "
 
+    # series column format
+    sql3 = """SELECT series_title
+    FROM series ORDER BY Length(series_title) desc LIMIT 1;"""
+    cursor.execute(sql3)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        slg = len(x)
+        ssp = (slg-6) * " "
+
     # author column format
-    sql2 = f"SELECT name FROM author WHERE author == {authorid} ORDER BY Length(name) desc LIMIT 1;"
+    sql2 = f"SELECT name FROM author WHERE id == {authorid} ORDER BY Length(name) desc LIMIT 1;"
     cursor.execute(sql2)
     results = cursor.fetchone()
     for i in results:
         x = (f"{i}")
         alg = len(x)
-        asp = (alg-10) * " "
+        asp = (alg-6) * " "
 
     # books by author
     sqlb = f"""SELECT Books.title, Author.name, series.series_title, books.availability 
@@ -358,8 +368,9 @@ LEFT JOIN Series ON Books.series = Series.id
 WHERE author = {authorid};"""
     cursor.execute(sqlb)
     results = cursor.fetchall()
+    print(f"Title {tsp}| Author {asp}| Series {ssp}| Availability")
     for i in results:
-        print(i)
+        print(f"{i[0]:{tlg}} | {i[1]:{alg}} | {i[2]:{slg}} | {i[3]}")
     
     # print table
     #sql = f"""SELECT Author.name, Books.title FROM Books LEFT JOIN Author ON Books.author = Author.id WHERE Books.book_id = {book_id};"""
