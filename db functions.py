ceaf920c2053e875ea75e0460d98d14043917fcc
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-28 12:45:23 +1300

    deleted unnesscary functions

--- db functions.py
+++ db functions.py
@@ -1,8 +1,6 @@
 import sqlite3
 
 # functions
-
-
 def fetch_author_id():
     db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
@@ -71,58 +69,6 @@ def fetch_all_books():
         print(f"{i[0]:>7} | {i[1]:{tlg}} | {i[2]:{alg}} | {i[3]:{slg}} | {i[4]}")
     db.close()
 
-
-def fetch_all_stand_alone_books():
-    db = sqlite3.connect('PraticeHome.db')
-    cursor = db.cursor()
-
-    # title column format
-    sql1 = """SELECT Title FROM Books 
-WHERE Series = 7 
-ORDER BY Length(title) desc LIMIT 1;"""
-    cursor.execute(sql1)
-    results = cursor.fetchone()
-    for i in results:
-        x = (f"{i}")
-        tlg = len(x)
-        tsp = (tlg-5) * " "
-
-    # author column format
-    sql2 = """SELECT Author.name FROM books
-LEFT JOIN Author ON Author.id = books.author
-WHERE series = 7 
-ORDER BY Length(name) desc LIMIT 1;"""
-    cursor.execute(sql2)
-    results = cursor.fetchone()
-    for i in results:
-        x = (f"{i}")
-        alg = len(x)
-        asp = (alg-6) * " "
-
-    # series column format
-    sql3 = """SELECT series_title FROM series
-WHERE id == 7
-ORDER BY Length(series_title) desc LIMIT 1"""
-    cursor.execute(sql3)
-    results = cursor.fetchone()
-    for i in results:
-        x = (f"{i}")
-        slg = len(x)
-        ssp = (slg-6) * " "
-
-    # print table
-    sql = """SELECT Books.book_id, Books.Title,
-Author.name, Series.series_title, Books.availability
-FROM Books
-LEFT JOIN Author ON Books.Author = Author.id
-LEFT JOIN Series ON Books.series = Series.id
-WHERE Series = 7;"""
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print(f"Book ID | Title {tsp}| Author {asp}| Series {ssp}| Avaiability")
-    for i in results:
-        print(f"{i[0]:>7} | {i[1]:{tlg}} | {i[2]:{alg}} | {i[3]:{slg}} | {i[4]}")
-    db.close()
 
 def fetch_all_unavaliable_books():
     db = sqlite3.connect('PraticeHome.db')
@@ -398,108 +344,6 @@ ORDER BY Length(title) desc LIMIT 1;"""
     db.close()
 
 
-def fetch_specfic_borrowing_table(book_id):
-    db = sqlite3.connect('PraticeHome.db')
-    cursor = db.cursor()
-
-    # forename column format
-    sql1 = """SELECT forename FROM Members
-    ORDER BY Length(forename) desc LIMIT 1;"""
-    cursor.execute(sql1)
-    results = cursor.fetchone()
-    for i in results:
-        x = (f"{i}")
-        fnlg = len(x)
-        fnsp = (fnlg-6) * " "
-
-    # title column format
-    sql2 = """SELECT title FROM books 
-ORDER BY Length(title) desc LIMIT 1;"""
-    cursor.execute(sql2)
-    results = cursor.fetchone()
-    for i in results:
-        x = (f"{i}")
-        tlg = len(x)
-        tsp = (tlg-10) * " "
-
-    # print table
-    sql = f"""SELECT Members.forename, Books.title,
-    borrowing.issue_date, Borrowing.due_date
-    FROM Borrowing
-    LEFT JOIN Members ON borrowing.member_id = Members.member_id
-    LEFT JOIN Books ON borrowing.book_id = Books.book_id
-    WHERE Borrowing.book_id = {book};"""
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print(f"Member {fnsp}| Book Title {tsp}| Due Date | Issue Date")
-    for i in results:
-        print(f"{i[0]:{fnlg}} | {i[1]:{tlg}} | {i[2]:} | {i[3]} ")
-    db.close()
-
-
-def fetch_books_by_specific_author(book_id):
-    db = sqlite3.connect('PraticeHome.db')
-    cursor = db.cursor()
-
-    # author id
-    sqla = f"""SELECT author FROM Books 
-WHERE book_id = {book_id};"""
-    cursor.execute(sqla)
-    results = cursor.fetchone()
-    for i in results:
-        authorid = i
-
-    # title column format
-    sql1 = """SELECT title FROM books 
-ORDER BY Length(title) desc LIMIT 1;"""
-    cursor.execute(sql1)
-    results = cursor.fetchone()
-    for i in results:
-        x = (f"{i}")
-        tlg = len(x)
-        tsp = (tlg-5) * " "
-
-    # series column format
-    sql3 = """SELECT series_title
-    FROM series ORDER BY Length(series_title) desc LIMIT 1;"""
-    cursor.execute(sql3)
-    results = cursor.fetchone()
-    for i in results:
-        x = (f"{i}")
-        slg = len(x)
-        ssp = (slg-6) * " "
-
-    # author column format
-    sql2 = f"SELECT name FROM author WHERE id == {authorid} ORDER BY Length(name) desc LIMIT 1;"
-    cursor.execute(sql2)
-    results = cursor.fetchone()
-    for i in results:
-        x = (f"{i}")
-        alg = len(x)
-        asp = (alg-6) * " "
-
-    # books by author
-    sqlb = f"""SELECT Books.title, Author.name, series.series_title, books.availability 
-FROM Books
-LEFT JOIN Author ON Books.author = Author.id
-LEFT JOIN Series ON Books.series = Series.id
-WHERE author = {authorid};"""
-    cursor.execute(sqlb)
-    results = cursor.fetchall()
-    print(f"Title {tsp}| Author {asp}| Series {ssp}| Availability")
-    for i in results:
-        print(f"{i[0]:{tlg}} | {i[1]:{alg}} | {i[2]:{slg}} | {i[3]}")
-
-    # print table
-    # sql = f"""SELECT Author.name, Books.title FROM Books LEFT JOIN Author ON Books.author = Author.id WHERE Books.book_id = {book_id};"""
-    # cursor.execute(sql)
-    # results = cursor.fetchall()
-    # print(f"Author {asp}| Book Title {tsp}")
-    # for i in results:
-    #     print(f"{i[0]:{alg}} | {i[1]:{tlg}}")
-    db.close()
-
-
 def fetch_specific_series(id):
     db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
@@ -555,6 +399,7 @@ WHERE series = {id};"""
     for i in results:
         print(f"{i[0]:{tlg}} | {i[1]:{alg}} | {i[2]:{slg}} | {i[3]}")
     db.close
+
 
 # main code
 print("\nWelcome to Libaray Database\n")
